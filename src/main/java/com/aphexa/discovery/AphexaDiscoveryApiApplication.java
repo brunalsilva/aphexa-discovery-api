f056bd2394fceec39ad8c310ced7f5dcee86a5d7
Bruna <beyss21@gmail.com>
2025-06-03 12:59:01 -0300

    chore: project initial structure with spring boot and flyway

---
+++ src/main/java/com/aphexa/discovery/AphexaDiscoveryApiApplication.java
@@ -0,0 +1,14 @@
+package com.aphexa.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AphexaDiscoveryApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AphexaDiscoveryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aphexa/discovery/AphexaDiscoveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aphexa.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AphexaDiscoveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
